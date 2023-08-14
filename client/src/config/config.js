836747a588af0be6d2bc533d2ea3f1b564538da0
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2023-08-14 15:22:29 +0100

    hot fix

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
       backendUrl: "http://localhost:8080/api/v1",
     },
     production: {
-      backendUrl: "https://spacee-api.onrender.com",
+      backendUrl: "https://spacee-api.onrender.com/api/v1",
     },
   };
   
